98dc317a84976632c1cc694741eac751b490a56d
Erin Stevenson <estevenson_94@live.com>
2019-01-02 17:19:08 +0000

    Refactored leap_years_simple.rb

--- basic_kata/leap_year_project/lib/leap_years_simple.rb
+++ basic_kata/leap_year_project/lib/leap_years_simple.rb
@@ -1,13 +1,3 @@
 def leap_year(year)
-  if year % 4 != 0
-    false
-  elsif year % 100 == 0 && year % 400 != 0
-    false
-  elsif year % 4 == 0 && year % 100 != 0
-    true
-  elsif year % 400 == 0
-    true
-  else 
-    false
-  end
+  (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0)
 end
