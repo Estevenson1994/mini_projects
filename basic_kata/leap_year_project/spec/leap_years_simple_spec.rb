bf1ac3d64f3c506079d24e8e614755dec1753b6f
Erin Stevenson <estevenson_94@live.com>
2019-01-02 17:09:59 +0000

    Wrote leap_years_simple.rb to pass tests

--- basic_kata/leap_year_project/spec/leap_years_simple_spec.rb
+++ basic_kata/leap_year_project/spec/leap_years_simple_spec.rb
@@ -4,4 +4,16 @@ describe 'leap_years_simple' do
   it 'return true when passed 2000' do
     expect(leap_year(2000)).to eq true
   end
+  it 'return true when passed 2000' do
+    expect(leap_year(1970)).to eq false
+  end
+  it 'return true when passed 2000' do
+    expect(leap_year(1900)).to eq false
+  end
+  it 'return true when passed 2000' do
+    expect(leap_year(1988)).to eq true
+  end
+  it 'return true when passed 2000' do
+    expect(leap_year(1500)).to eq false
+  end
 end
